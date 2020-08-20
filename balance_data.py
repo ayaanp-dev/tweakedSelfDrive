244ab0777200bb7937ea790ee565e57563dc3d82
Ayaan Panda <ayaan.panda1@gmail.com>
2020-08-20 08:28:07 -0400

    Using more keys

--- balance_data.py
+++ balance_data.py
@@ -3,16 +3,21 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 
-train_data = np.load('trainingdata/training_data.npy', allow_pickle=True)
+train_data = np.load('training_data.npy', allow_pickle=True)
 
 df = pd.DataFrame(train_data)
 print(df.head())
 print(Counter(df[1].apply(str)))
 
-lefts = []
-rights = []
-forwards = []
-backwards = []
+w = []
+a = []
+s = []
+d = []
+wa = []
+wd = []
+sa = []
+sd = []
+nk = []
 
 shuffle(train_data)
 
@@ -20,24 +25,48 @@ for data in train_data:
     img = data[0]
     choice = data[1]
 
-    if choice == [1,0,0,0]:
-        lefts.append([img,choice])
-    elif choice == [0,1,0,0]:
-        forwards.append([img,choice])
-    elif choice == [0,0,1,0]:
-        rights.append([img,choice])
-    elif choice == [0,0,0,1]:
-        backwards.append([img, choice])
+    if choice == [1,0,0,0,0,0,0,0,0]:
+        w.append([img,choice])
+        
+    elif choice == [0,1,0,0,0,0,0,0,0]:
+        a.append([img,choice])
+        
+    elif choice == [0,0,1,0,0,0,0,0,0]:
+        s.append([img,choice])
+        
+    elif choice == [0,0,0,1,0,0,0,0,0]:
+        d.append([img,choice])
+        
+    elif choice == [0,0,0,0,1,0,0,0,0]:
+        wa.append([img,choice])
+        
+    elif choice == [0,0,0,0,0,1,0,0,0]:
+        wd.append([img,choice])
+        
+    elif choice == [0,0,0,0,0,0,1,0,0]:
+        sa.append([img,choice])
+        
+    elif choice == [0,0,0,0,0,0,0,1,0]:
+        sd.append([img,choice])
+        
+    elif choice == [0,0,0,0,0,0,0,0,1]:
+        nk.append([img,choice])
+
     else:
-        print('no matches')
+        print("no matches")
 
 
-forwards = forwards[:len(lefts)][:len(rights)]
-lefts = lefts[:len(forwards)]
-rights = rights[:len(forwards)]
-backwards = backwards[:len(lefts)][:len(rights)]
+##w = w[:len(s)][:len(a)][:len(d)][:len(wa)][:len(wd)][:len(sa)][:len(sd)]
+##s = s[:len(w)]
+##a = a[:len(w)]
+##d = d[:len(w)]
+##wa = wa[:len(w)]
+##wd = wd[:len(w)]
+##sa = sa[:len(w)]
+##sd = sd[:len(w)]
+##nk = nk[:len(w)]
 
-final_data = forwards + lefts + rights + backwards
+final_data = w + a + s + d + wa + wd + sa + sd + nk
 shuffle(final_data)
 
-np.save('trainingdata/balanced_training_data.npy', final_data)
+np.save('balanced_data.npy', final_data)
---
+++ create_data.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+#from getkeys import key_check
+from getscreen import get_screen
+import os
+import time
+import keyboard
+
+def keys_to_output():
+
+    w = [1,0,0,0,0,0,0,0,0]
+    a = [0,1,0,0,0,0,0,0,0]
+    s = [0,0,1,0,0,0,0,0,0]
+    d = [0,0,0,1,0,0,0,0,0]
+    wa = [0,0,0,0,1,0,0,0,0]
+    wd = [0,0,0,0,0,1,0,0,0]
+    sa = [0,0,0,0,0,0,1,0,0]
+    sd = [0,0,0,0,0,0,0,1,0]
+    nk = [0,0,0,0,0,0,0,0,1]
+    
+    output = [0,0,0,0,0,0,0,0]
+
+    if keyboard.is_pressed("w"):
+        output = w
+    elif keyboard.is_pressed("a"):
+        output = a
+    elif keyboard.is_pressed("s"):
+        output = s
+    elif keyboard.is_pressed("d"):
+        output = d
+    elif keyboard.is_pressed("w") and keyboard.is_pressed("a"):
+        output = wa
+    elif keyboard.is_pressed("w") and keyboard.is_pressed("d"):
+        output = wd
+    elif keyboard.is_pressed("s") and keyboard.is_pressed("a"):
+        output = sa
+    elif keyboard.is_pressed("s") and keyboard.is_pressed("d"):
+        output = sd
+    else:
+        output = nk
+
+    return output
+
+file_name = "training_data.npy"
+
+if os.path.isfile(file_name):
+    print("File exists, loading previous data!")
+    training_data = list(np.load(file_name, allow_pickle=True))
+else:
+    print("No file found, starting fresh!")
+    training_data = []
+
+def main():
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+        
+    while(True):
+        screen = get_screen(region=(0,40,800,600))
+        last_time = time.time()
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        screen = cv2.resize(screen, (80,60))
+        output = keys_to_output()
+        training_data.append([screen, output])
+        
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+        if len(training_data) % 500 == 0:
+            print(len(training_data))
+            np.save(file_name,training_data)
+
+main()
